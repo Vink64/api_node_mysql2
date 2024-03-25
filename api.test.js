dacf9411e21f20288d1ed53137dfb6e3e668fee4
Vink64 <161389498+Vink64@users.noreply.github.com>
2024-03-25 19:10:03 -0300

    Update api.test.js

--- api.test.js
+++ api.test.js
@@ -1,6 +1,8 @@
 const supertest = require('supertest');
 const {app,server,connection} = require('./index.js'); // Importe seu aplicativo Express
 
+var idCriado;
+
 describe('Teste Get /users', () => {
   it('deve responder com status 200', async () => {
     const response = await supertest(app).get('/users');
@@ -9,29 +11,35 @@ describe('Teste Get /users', () => {
     expect(response.body).toBeInstanceOf(Array);
     response.body.forEach(element => {
       expect(element).toHaveProperty("name");
+
     });
 
   });
 });
 
 describe('Teste Post /users', () => {
-  it('deve responder com status 201', async () => {
-    const response = await supertest(app).post('/users').send({name: "Juninho",email: "juninhodograu@gmail.com"});
+ it('deve responder com status 201', async () => {
+    const response = await supertest(app).post('/users').send({ name: "Fulano de Tal", email: "fulano@fulano.com" });
     expect(response.statusCode).toBe(201);
+    console.log(response.body);
+    idCriado = response.body.id;
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.id).toBeGreaterThan(0);
+    console.log("O id criado foi " + response.body.id);
     
   });
 });
 
 describe('Atualiza usuario Put /users', () => {
   it('deve responder com status 204', async () => {
-    const response = await supertest(app).post('/users/1').put({name: "Juninho",email: "juninhodograu@gmail.com"});
+    const response = await supertest(app).put('/users/1').send({ name: "Beltrano de Tal", email: "fulano@fulano.com" });
     expect(response.statusCode).toBe(204);
   });
 });
 
 describe('Deleta usuario Delete /users', () => {
   it('deve responder com status 204', async () => {
-    const response = await supertest(app).post('/users/1').delete({name: "Juninho",email: "juninhodograu@gmail.com"});
+    const response = await supertest(app).delete('/users/'+ idCriado);
     expect(response.statusCode).toBe(204);
   });
 });
